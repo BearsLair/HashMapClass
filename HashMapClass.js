4dacbbcebdfdb5dfa04232c4ef92dbae871efe98
Patrick Gross <142960751+BearsLair@users.noreply.github.com>
2025-09-19 18:06:33 -0700

    set up hashmap construtor, and has function

--- HashMapClass.js
+++ HashMapClass.js
@@ -1,1 +1,17 @@
-Hashmap(loadFactor, capacity) {}
+class Hashmap {
+  constructor(oadFactor, capacity = 16) {
+    this.buckets = [];
+    this.loadFactor = loadFactor;
+    this.capacity = capacity;
+  }
+}
+
+function hash(key, capacity = 16) {
+  let hashCode = 0;
+
+  const primeNumber = 31;
+  for (let i = 0; i < key.length; i++) {
+    hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % capacity;
+  }
+  return hashCode;
+}
